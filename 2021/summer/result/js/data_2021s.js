6b096a729bf55b992f40b303babe84d3641884fe
Hajime Koeda <Hajime Koeda hajime_koeda@dwango.co.jp>
2021-09-29 16:26:50 +0900

    add 2021 summer result data

---
+++ 2021/summer/result/js/data_2021s.js
@@ -0,0 +1,412 @@
+const datas = [
+  {
+    "category": "N予備校",
+    "id": 1,
+    "award": "健闘賞",
+    "name": "ミニミニコ",
+    "title": "ことばレインボー",
+    "desc": "小学生向けに語彙力を伸ばすために役立つアプリを作ってみました。 「気持ちを表す言葉」や「学習に役立つ言葉」などボタンを押すと、次々に言葉が出てきます。 初めはただ眺めるだけ、慣れてきたら口に出して読んだり、意味を調べたり、短文を作ったりします。 学年ごとにボタンが分かれているので、自分の学年に応じた学びができます。 虹の光を浴びるように、さまざまな言葉にふれて、語彙力をUPさせましょう！",
+    "page": "https://chikako2020.github.io/kotobaRainbow/",
+    "source": "https://github.com/chikako2020/kotobaRainbow"
+  },
+  {
+    "category": "N予備校",
+    "id": 2,
+    "award": "健闘賞",
+    "name": "まさし",
+    "title": "じゃんけんゲーム",
+    "desc": "じゃんけんゲームです。グーで勝つと3段(グリコ)， チョキで勝つと6段(チョコレイト)，パーで勝つと6段(パイナップル)登ります。先に15段登った方が勝ちです。",
+    "page": "https://ma3shi.github.io/janken/",
+    "source": "https://github.com/ma3shi/janken/tree/gh-pages"
+  },
+  {
+    "category": "N予備校",
+    "id": 3,
+    "award": "健闘賞",
+    "name": "まさし",
+    "title": "数字記憶ゲーム",
+    "desc": "数字記憶ゲームです。 spinボタンを押すとカードが1枚ずつ回転し、裏側に隠された数字が一瞬見えます。 questionボタンを押すと数字が出題されます。その数字カードを選んでください。 全部で3ステージあります。",
+    "page": "https://ma3shi.github.io/memory-game/",
+    "source": "https://github.com/ma3shi/memory-game/tree/gh-pages"
+  },
+  {
+    "category": "N予備校",
+    "id": 4,
+    "award": "技術賞",
+    "name": "まさし",
+    "title": "10パズル",
+    "desc": "4つの数字と四則演算（＋、－、×、÷）を使って10にするテンパズルです。  全部で15ステージあります。",
+    "page": "https://ma3shi.github.io/tenpuzzle/",
+    "source": "https://github.com/ma3shi/tenpuzzle/tree/gh-pages"
+  },
+  {
+    "category": "N予備校",
+    "id": 5,
+    "award": "健闘賞",
+    "name": "まさし",
+    "title": "2.3.4枚めくり",
+    "desc": "神経衰弱です。 3種類のカード(ニワトリ:２枚、サンタ:3枚、四つ葉のクローバー:4枚)をそれぞれ合わせてください. ミスをすると残り回数が減ります。残り回数が0になるとゲームオーバーです。",
+    "page": "https://ma3shi.github.io/card-memory/",
+    "source": "https://github.com/ma3shi/card-memory/tree/gh-pages"
+  },
+  {
+    "category": "N予備校",
+    "id": 6,
+    "award": "健闘賞",
+    "name": "まさし",
+    "title": "ルーレット",
+    "desc": "ルーレットゲームです。ビンクが当たりです。3回失敗するとゲームオーバーです。",
+    "page": "https://ma3shi.github.io/roulette/",
+    "source": "https://github.com/ma3shi/roulette/tree/gh-pages"
+  },
+  {
+    "category": "N予備校",
+    "id": 7,
+    "award": "健闘賞",
+    "name": "まさし",
+    "title": "todoリスト",
+    "desc": "todoリストです。",
+    "page": "https://ma3shi.github.io/todo/",
+    "source": "https://github.com/ma3shi/todo/tree/gh-pages"
+  },
+  {
+    "category": "N予備校",
+    "id": 8,
+    "award": "健闘賞",
+    "name": "まさし",
+    "title": "秒当てゲーム",
+    "desc": "時間当てゲームです。 誤差の範囲内でストップできたら成功です。 1回ごとに目標タイムと許容誤差は変わります。",
+    "page": "https://ma3shi.github.io/seconds-guess/",
+    "source": "https://github.com/ma3shi/seconds-guess/tree/gh-pages"
+  },
+  {
+    "category": "N予備校",
+    "id": 9,
+    "award": "健闘賞",
+    "name": "まさし",
+    "title": "数字当てゲーム",
+    "desc": "1から100の間に隠された当たりの数字を探します。 選択した数字が間違っていた場合、当たり数字より大きいか小さいかヒントが出ます。 10回以内に当たり数字を見つければ正解です。",
+    "page": "https://ma3shi.github.io/number-guess/",
+    "source": "https://github.com/ma3shi/number-guess/tree/gh-pages"
+  },
+  {
+    "category": "N予備校",
+    "id": 10,
+    "award": "健闘賞",
+    "name": "りさりさと",
+    "title": "N予備校クイズ",
+    "desc": "N予備校にまつわるクイズに答える作品です。 (個人ネタですのでご承知ください)",
+    "page": "https://risarisato.github.io/SummerContest/index05.html",
+    "source": "https://github.com/risarisato/SummerContest/tree/gh-pages"
+  },
+  {
+    "category": "N予備校",
+    "id": 11,
+    "award": "健闘賞",
+    "name": "HappyK",
+    "title": "ランダム Emoji",
+    "desc": "オブジェクトの授業内での「時間あてゲーム」を、PCの他にモバイル端末でも遊べるようにしたくて作りました。 onclick-areaにランダムに出る絵文字を、これもまたリロード毎にランダムで出題される秒数になったらマウスクリックまたはタップすることによって、display-areaに結果秒数が表示されます。 また、当てる秒数に近い順に 「おめでとう」紙吹雪 「すばらしい」絵文字の雨 「おしい」絵文字ひとつが回転しながら降りていく という反応表示を付けました。  Safariですが、iPhone と iPad での動作は確認できました。 再スタートはリロードです。 一見シンプル、しかしランダムに変化するゲームを目指しました。",
+    "page": "https://happykumamoto.github.io/RandomEmoji/",
+    "source": "https://github.com/HappyKumamoto/RandomEmoji"
+  },
+  {
+    "category": "N予備校",
+    "id": 12,
+    "award": "健闘賞",
+    "name": "pala",
+    "title": "Perfect Prefecture",
+    "desc": "各都道府県の特徴や県庁所在地を覚えてもらおうというページです。",
+    "page": "https://89nno.github.io/PerfectPrefecture/index.html",
+    "source": "https://github.com/89nno/PerfectPrefecture/tree/gh-pages"
+  },
+  {
+    "category": "N予備校",
+    "id": 13,
+    "award": "最優秀賞",
+    "name": "こってぃ",
+    "title": "コミュニケーションタイプ診断",
+    "desc": "コミュニケーションタイプを診断できるアプリケーションです。",
+    "page": "https://coticoticotty.github.io/communication-type-assessment/",
+    "source": "https://github.com/coticoticotty/communication-type-assessment"
+  },
+  {
+    "category": "N予備校",
+    "id": 14,
+    "award": "健闘賞",
+    "name": "Taka",
+    "title": "休暇割り振り君",
+    "desc": "私の職場では定期的に年末までの休暇計画を提出する必要があるため、休暇をなるべく均等な間隔で取得する計画を立てるアプリを作りました。",
+    "page": "https://hamuyashiki.github.io/calculate-vacation-date/schedule",
+    "source": "https://github.com/hamuyashiki/calculate-vacation-date.git"
+  },
+  {
+    "category": "N予備校",
+    "id": 15,
+    "award": "健闘賞",
+    "name": "BlueZhiaar",
+    "title": "夏休みの絵日記ジェネレーター",
+    "desc": "夏休みの絵日記風の文章と画像を自動で作成できるページです。",
+    "page": "https://bluezhiaar.github.io/summer-diary/index.html",
+    "source": "https://github.com/BlueZhiaar/summer-diary/tree/gh-pages"
+  },
+  {
+    "category": "N予備校",
+    "id": 16,
+    "award": "健闘賞",
+    "name": "fleur-hana",
+    "title": "和の伝統色　読めるかな？",
+    "desc": "日本の伝統色の読み方を答えるアプリです。 ボタンを押すとランダムに問題が表示されるので、制限時間内に解答欄へ入力してください。正解すると色名についての説明が表示されます。 操作方法など、詳しい説明はREAD.mdファイルに書きました。",
+    "page": "https://fleur-hana.github.io/webAppContest-summer-2021/contest.html",
+    "source": "https://github.com/fleur-hana/webAppContest-summer-2021/tree/main"
+  },
+  {
+    "category": "N予備校",
+    "id": 17,
+    "award": "健闘賞",
+    "name": "kofun0117",
+    "title": "抽選箱",
+    "desc": "好きなものを入れて抽選することができるページです",
+    "page": "https://kofun0117.github.io/lottery/lottery.html",
+    "source": "https://github.com/kofun0117/lottery/tree/gh-pages"
+  },
+  {
+    "category": "N予備校",
+    "id": 18,
+    "award": "健闘賞",
+    "name": "にょろぼん",
+    "title": "covid19　VRCalc (covid19ワクチン検討用リスクチェッカー）",
+    "desc": "covid19ワクチン摂取検討の参考となるリアルタイムの 感染致死リスク情報を簡単な項目入力でチェック出来る webアプリです。",
+    "page": "https://nyeoro-bong.github.io/vaccineRiskCalc/VRCalc.html",
+    "source": "https://nyeoro-bong.github.io/vaccineRiskCalc"
+  },
+  {
+    "category": "N予備校",
+    "id": 19,
+    "award": "健闘賞",
+    "name": "にゃんだーZ",
+    "title": "結婚披露宴おまかせ選曲",
+    "desc": "結婚披露宴の曲を決める時間がなくこだわりもないという方に、ボタンひとつでランダムに曲を選べるサイトを作りました。",
+    "page": "https://aknkz.github.io/sel-wsong/sel-wsong.html",
+    "source": "https://github.com/aknkz/sel-wsong"
+  },
+  {
+    "category": "N予備校",
+    "id": 20,
+    "award": "動くで賞",
+    "name": "nakako",
+    "title": "猫のお掃除大作戦",
+    "desc": "猫を操作して家具を避けながらゴミ拾いをするゲーム。 特別授業のゲーム作りが楽しかったのでアレンジしてみました。",
+    "page": "https://nakako.github.io/catCleaner/catCleaner.html",
+    "source": "https://github.com/nakako/catCleaner"
+  },
+  {
+    "category": "N予備校",
+    "id": 21,
+    "award": "デザイン賞",
+    "name": "ふーちん",
+    "title": "顔文字制作!(^^)!",
+    "desc": "ネットで「顔文字」と検索したら面白い顔文字がたくさん出てきました。しかし機種依存文字を使っているものが多いので、機種依存の使われていない顔文字をランダムに作ってくれるページを制作しました。気に入った顔文字はダウンロード形式で登録できます。「今日の顔文字」は特定の日には特定の顔文字になります。おすすめはクリスマスの日の「今日の顔文字」です。あとコピーボタンも実装したかったですが、時間がなくてできませんでした。",
+    "page": "https://fuuchin.github.io/kaomoji-emoticon/index.html",
+    "source": "https://github.com/fuuchin/kaomoji-emoticon/tree/gh-pages"
+  },
+  {
+    "category": "N予備校",
+    "id": 22,
+    "award": "健闘賞",
+    "name": "kenton116",
+    "title": "Favorite PictureViewer",
+    "desc": "画像をブラウザ上で一時的に管理できるサービスです。 メモ機能と拡大機能を実装しました。 複数行書くことができるメモは、写真のタイトルや撮影地など、なんでも書き留めておけます。 例えば、料理の画像にレシピのメモをつけておけば簡単にレシピ本のようにまとめることができますし、プレゼンテーションの画像に発表原稿のメモをつけたらプレゼンの練習アプリにもなります。 様々な用途に使用できるシンプルな画像ビューアーです。",
+    "page": "https://kenton116.github.io/Favorite-PictureViewer/main.html",
+    "source": "https://github.com/kenton116/Favorite-PictureViewer"
+  },
+  {
+    "category": "N予備校",
+    "id": 23,
+    "award": "健闘賞",
+    "name": "roo",
+    "title": "Pay It Forward Maker",
+    "desc": "・先に率先して良いことをすると、回りまわって還ってくる ・思いやりの輪が広がって、生きやすい社会としての一助となる  という考え方を \"Pay It Forward\" と言うそうです。   一日一善、なにか良いことをしたら自分に返ってくるかも...!",
+    "page": "https://cat22roo.github.io/PayItForward-Maker/",
+    "source": "https://github.com/cat22roo/PayItForward-Maker"
+  },
+  {
+    "category": "N予備校",
+    "id": 24,
+    "award": "健闘賞",
+    "name": "ゲストさん",
+    "title": "つくりかけいぼ",
+    "desc": "家計簿っぽいものを作ろうとしているところ",
+    "page": "https://compmac-sblo.github.io/NConSummer2021-1/",
+    "source": "https://github.com/compmac-sblo/NConSummer2021-1"
+  },
+  {
+    "category": "N予備校",
+    "id": 25,
+    "award": "健闘賞",
+    "name": "hanaP",
+    "title": "もぎせか答案作成",
+    "desc": "もぎせかの答案作成用に作りました。 入力文字数をカウントします。",
+    "page": "https://hana-pr.github.io/contest/touan.html",
+    "source": "https://github.com/hana-pr/contest"
+  },
+  {
+    "category": "N高等学校",
+    "id": 26,
+    "award": "健闘賞",
+    "name": "たぬみく",
+    "title": "あざらしくんの１日",
+    "desc": "コードをまとめるところを頑張りました！！",
+    "page": "https://mikurui.github.io/azarashikun/azarashikun.html",
+    "source": "https://github.com/mikurui/azarashikun.git"
+  },
+  {
+    "category": "N高等学校",
+    "id": 27,
+    "award": "健闘賞",
+    "name": "たぬきっつぁん",
+    "title": "ランダムカラーピッカー",
+    "desc": "Webサイトを作っているときに、いい感じの色を勝手に決めてくれるアプリが欲しかったので作りました。\n生成するボタンかEnterキーを押すだけでランダムに色が生成されます。また赤系、緑系、青系、モノトーンのボタンでは、それぞれに近い色を生成することもできます。カラーコード下のボタンを押すとブラウザ標準のカラーピッカーが利用でき、色の調整などができます。またつぶやくボタンでは生成されたカラーコードをツイートできます（カラーコードの#とハッシュタグの#をかけてます）\n頑張ったところはカラーコードの生成コードです。特定の色に近い色を生成する方法を見つけるのに苦労しました。そのおかげでカラーコードの仕組みが少し理解できました。\nWeb制作以外にも画像編集などさまざまな場面で活用できると思います。また単純にEnterキーを連打したり、長押しして色の変化を見たりするだけでも楽しいです！",
+    "page": "https://app.tanukizzan.com/random-color/",
+    "source": "https://github.com/tanukizzan/random-color"
+  },
+  {
+    "category": "N高等学校",
+    "id": 28,
+    "award": "健闘賞",
+    "name": "take",
+    "title": "ぼくの単語帳",
+    "desc": "単語を作り保存できるアプリです。アプリ内でフォルダーを作成することができ、単語を自分なりに分類することができます。作成したフォルダーと単語はlocalstorageによって保存されるので消えません。単語は作るだけでなく、ゲーム感覚で覚えることができます。",
+    "page": "https://takeharu7-51.github.io/My-Vocabulary-Book/",
+    "source": "https://github.com/Takeharu7-51/My-Vocabulary-Book"
+  },
+  {
+    "category": "N高等学校",
+    "id": 29,
+    "award": "健闘賞",
+    "name": "ねこむら",
+    "title": "小泉構文メーカー",
+    "desc": "小泉進次郎が迷言をたくさん言っていたので作ってみました",
+    "page": "https://11201ozi.github.io/sinjiro/sinjiro.html",
+    "source": "https://github.com/11201ozi/sinjiro/tree/gh-pages"
+  },
+  {
+    "category": "N中等部",
+    "id": 30,
+    "award": "最優秀賞",
+    "name": "Yuta Kobayashi",
+    "title": "便利なウェブツール",
+    "desc": "シンプルなウェブツールがまとまったサイトが欲しかったので制作しました。文字数カウンターやYoutubeのサムネイルを取得できるツールなどなど用意しています。HTML/CSS/JSという技術だけで再現するのが思った以上に難しく様々なソースコードを参考にさせて頂きました。",
+    "page": "https://progedu.github.io/web-tools/",
+    "source": "https://github.com/progedu/web-tools"
+  },
+  {
+    "category": "N高等学校",
+    "id": 31,
+    "award": "健闘賞",
+    "name": "sol",
+    "title": "支払いアプリ / payment-app",
+    "desc": "友人などと食事や買い物に行った際に、誰が支払うかや1人当たりの支払い金額を計算するのが少し億劫だったので制作してみました。割り勘機能のみだと電卓があるので必要性が低いと思い、誰が支払うのかランダムで決めれる奢り機能も実装しました。デザインはシンプルさを意識しながら制作しました。システムは軽さを重視するためにクラスを使い、htmlファイル1つで完結させました。JavaScriptはあまり得意ではなかったのですが、色々調べたり試行錯誤してなんとか完成させることができました。初めて使ってみたプログラムがたくさんあるので、今後のサイト制作に活用していけたらいいなと思っています。",
+    "page": "https://loveruvwnsol.github.io/payment-app/",
+    "source": "https://github.com/loveruvwnSol/payment-app"
+  },
+  {
+    "category": "N高等学校",
+    "id": 32,
+    "award": "健闘賞",
+    "name": "ねや",
+    "title": "寝屋川公園駅発車標",
+    "desc": "何故作り始めたかは自分でも謎ですが、大阪府内にある小さな駅「寝屋川公園駅」の発車標を作りました。\n拘ったところは、実際の駅に設置されている発車標にできる限り似せたところと、リアルタイムで更新されるようにしたところです。\n現在時刻から次の列車を調べるプログラムを作成するのに苦労しました。",
+    "page": "https://neyatetsu.github.io/Neyagawakoen/",
+    "source": "https://github.com/NeyaTetsu/Neyagawakoen"
+  },
+  {
+    "category": "N高等学校",
+    "id": 33,
+    "award": "健闘賞",
+    "name": "syoujin",
+    "title": "関西人診断",
+    "desc": "途中までです。診断するボタンを消して乱数で20問から5問出題する処理を二回以上できなくする処理や、クイズとして成立させることができませんでしたが一番やりたかった部分である乱数で20問から5問出題する処理は渾身の出来です。",
+    "page": "https://syoujin.github.io/kansai-quiz/quiz.html",
+    "source": "https://github.com/syoujin/kansai-quiz"
+  },
+  {
+    "category": "S高等学校",
+    "id": 34,
+    "award": "健闘賞",
+    "name": "クッキー",
+    "title": "FizzBuzzを見る",
+    "desc": "制作の動機は、第１章で習ったことを応用して何か作りたいと思ったからです。\n機能は、ボタンでページを切り替えることができます。\nPRは、FizzBuzzを楽しく見ることがでます。FizzBuzzが順番に１つずつ出てくるので、見ながら自分もFizzBuzzをすることができます。\n頑張ったところは、カウントアプタイマーに文字を入れることが難しく、時間がかかりましたが自力で解決できました。",
+    "page": "https://marotyuket.github.io/FizzBuzz-watch/index1.html",
+    "source": "https://github.com/marotyuket/FizzBuzz-watch/tree/gh-pages"
+  },
+  {
+    "category": "N高等学校",
+    "id": 35,
+    "award": "最優秀賞",
+    "name": "golden peach",
+    "title": "鬼ごっこゲーム〜迫り来るおばけたち〜",
+    "desc": "機能説明\n30秒間お化けたちから逃げることができればクリアとなるゲームです！\nそれぞれのお化けを戻すことができるアイテムがあるので、それをうまく工夫して30秒間逃げ切ってください！（ヒント:おばけ→し◯、ドラキュラ→に◯に◯、鬼→◯め）\n12秒ごとに鬼が追加されていくので、時間経過に連れて難易度も上がっていきます。\nこのリンクに飛んだ瞬間からゲームが開始されます。\n\n制作動機\n私は今年からプログラミングを勉強し始めました。\nプログラミング初心者なので、簡単なサイトしか作ったことがありませんでしたが、\nゲームを作ってみたいと思いました。\n私が鬼ごっこゲームにハマっていたというのもあり、自分で鬼ごっこゲームを作りたいと思ったのでこのゲームを作りました。\n\nこれから\n本当は、スタート画面と説明画面、コンティニュー画面を作りたかったのですが、\n今の私の技術力では難しかったので、これからもどんどんこのゲームを良いものに改善していきたいなと思っています。スコアを作ってみるのも面白そうですよね！",
+    "page": "https://kplovepeach.github.io/game/index.html",
+    "source": "https://github.com/KPLOVEPeach/game/tree/gh-pages"
+  },
+  {
+    "category": "S高等学校",
+    "id": 36,
+    "award": "健闘賞",
+    "name": "らっく",
+    "title": "夜ご飯が思いつかないときはコレだ！！",
+    "desc": "「夜ご飯、何食べようかなぁ。」と考えていたらいつの間にかカップ麺になってしまうことが何回かありました。夜ご飯の何にするか考えるときに考えがまとまらないことが多いです。だったら頭の中で考えていることをまとめられるようにしたら役に立つんじゃないかと思ったので作ることにしました。\n　今回は「夜ご飯」をテーマにしましたが、頭の中がまとまらないことはほかにもあると思います。なので、ほかのテーマのものに応用しやすくなるように意識してコードを描きました。夜ご飯をどうするかの質問はこれから少しづつよくしていく予定です。\n　初めて作ったWebページでしたが、関数がどんなものか作っていくうちによくわかりました。これからも”思考整理系”のWebページシリーズを作っていこうと思います。何度も作り直していくたびにできていく様子がとても楽しかったです。\n　まだCSSに関してはJavaScriptに比べて学びが薄いのでこれからのWebページ作成の中でマナbbで行きたいと思います。",
+    "page": "https://github.com/RAKU0731/dinner-coaching.git",
+    "source": "https://raku0731.github.io/dinner-coaching/coaching-copy"
+  },
+  {
+    "category": "N高等学校",
+    "id": 37,
+    "award": "優秀賞",
+    "name": "かしわ餅tea",
+    "title": "お絵かきアニメーション",
+    "desc": "簡易的にお絵かきアニメーションを作成できるアプリです。Drag APIやNative File System APIを使って、シームレスに作成できるようにしました。",
+    "page": "https://kashitea.github.io/oekakiAnimation/",
+    "source": "https://github.com/kashitea/oekakiAnimation"
+  },
+  {
+    "category": "N高等学校",
+    "id": 38,
+    "award": "健闘賞",
+    "name": "ろずっち",
+    "title": "3DRPG",
+    "desc": "自作RPGを作ってみようと昔から思っていたので、まだ挑戦したことのない3Dの要素を取り入れてゲームを作りたいと思い作成しました。3Dに関するプログラム、その他判定やステージ、GUIの作成などゲームを作るのは大変だなと分かりました。まだ未完成ですが、基本的な動作はできます。",
+    "page": "https://amechankyobo.github.io/3D_RPG/",
+    "source": "https://github.com/amechanKyobo/3D_RPG"
+  },
+  {
+    "category": "N高等学校",
+    "id": 39,
+    "award": "健闘賞",
+    "name": "うさぎさん",
+    "title": "clock",
+    "desc": "切手のような数字のデザインを考えるのに一番苦労しました。普通の切手は絵がメインなので数字は小さいのが多く、今回のために数字をメインにデザインするのがとても大変でした。",
+    "page": "https://chiyogami.github.io/clock/clock.html",
+    "source": "https://github.com/chiyogami/clock/blob/gh-pages/clock.html、https://github.com/chiyogami/clock/blob/gh-pages/clock.js、https://github.com/chiyogami/clock/blob/gh-pages/clock.css、https://github.com/chiyogami/clock/tree/gh-pages/background、https://github.com/chiyogami/clock/tree/gh-pages/number、https://github.com/chiyogami/clock/tree/gh-pages/parts、https://github.com/chiyogami/clock/tree/gh-pages/week"
+  },
+  {
+    "category": "N高等学校",
+    "id": 40,
+    "award": "健闘賞",
+    "name": "yu1k",
+    "title": "IPアドレス情報確認くん",
+    "desc": "入力した IPv4 アドレス の情報を確認します。サブネットマスクの計算のみ対応。\n詳しくは READMEmd に記載します。",
+    "page": "https://yu1k.github.io/ip-network-info/",
+    "source": "https://github.com/yu1k/ip-network-info/tree/nyobikou-webappcontest-2021-summer/client/"
+  },
+  {
+    "category": "N高等学校",
+    "id": 41,
+    "award": "健闘賞",
+    "name": "parakeet",
+    "title": "notes",
+    "desc": "IndexedDB API、Notifications API、Web Workers API、Web Componentsなどのおもしろそうな機能を使ってみたくて作ったメモアプリです。contenteditable属性を利用したWYSIWYGエディタの実装が大変でした。作りたい機能が多すぎて締め切りに間に合いませんでした。",
+    "page": "https://parakeet15.github.io/notes/",
+    "source": "https://github.com/parakeet15/notes"
+  }
+]
---
+++ 2021/summer/result/js/template.js
@@ -0,0 +1,29 @@
+'use strict'
+
+function setAwardData(selecter, data) {
+    selecter.find(".data-thumb").attr("style", `background-image: url("assets/img/applications/${data.id}.png");`);
+    selecter.find(".data-title").text(data.title);
+    selecter.find(".data-page").attr("href", data.page);
+    selecter.find(".data-name").text(data.name);
+    selecter.find(".data-source").attr("href", data.source);
+    selecter.find(".data-desc").text(data.desc);
+}
+
+// 受賞者
+setAwardData($(".nhigh.grand-prize"), datas.filter( e => e.category === "N高等学校" && e.award === "最優秀賞")[0]);
+setAwardData($(".nhigh.first-place"), datas.filter( e => e.category === "N高等学校" && e.award === "優秀賞")[0]);
+setAwardData($(".nprog.grand-prize"), datas.filter( e => e.category === "N中等部" && e.award === "最優秀賞")[0]);
+setAwardData($(".nprep.grand-prize"), datas.filter( e => e.category === "N予備校" && e.award === "最優秀賞")[0]);
+setAwardData($(".nprep.tech-prize"), datas.filter( e => e.category === "N予備校" && e.award === "技術賞")[0]);
+setAwardData($(".nprep.move-prize"), datas.filter( e => e.category === "N予備校" && e.award === "動くで賞")[0]);
+setAwardData($(".nprep.design-prize"), datas.filter( e => e.category === "N予備校" && e.award === "デザイン賞")[0]);
+
+// 健闘賞
+const contentTemplate = $(".template");
+const kentos = datas.filter( e => e.award === "健闘賞" );
+for (let data of kentos) { 
+    const content = contentTemplate.clone(true);
+    setAwardData(content, data);
+    contentTemplate.after(content);
+}
+contentTemplate.hide();
